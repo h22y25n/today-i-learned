d6bb47500af1b50910cb2562e6504d3fcadce587
강희연 <k.h22y25n@gmail.com>
2020-06-29 16:58:23 +0900

    Resolved: svg loader 설치, 세팅, 사용

--- vue/svg-icon/src/App.vue
+++ vue/svg-icon/src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
+    <!-- svgicon -->
     <svgIconsDemo></svgIconsDemo>
     <span>Icon Demonstration in App.vue:</span>
-    <svgicon icon="ic_error_24px" width="18" height="18" color="#000"></svgicon>
+    <svgicon icon="ic_error_24px" width="60" height="60" color="#86cfee"></svgicon>
+    <!-- svg-loader -->
+    <svgIcon width="120" height="120" iconType="warning" iconColor="salmon" />
+    <svgIcon width="60" height="60" iconType="error_outline" iconColor="#f0c863" />
+    <svgIcon width="60" height="60" iconType="error" iconColor="#86cfee" />
   </div>
 </template>
 
 <script>
 import svgIconsDemo from "./components/svgIconsDemo.vue";
-import './compiled-icons/index.js';
+import "./compiled-icons/index.js";
+import svgIcon from "./components/svg-icon.vue";
 export default {
   name: "App",
   components: {
     svgIconsDemo,
-  },
+    svgIcon
+  }
 };
 </script>
 
---
+++ vue/svg-icon/src/components/svg-icon.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :style="iconStyle">
+    <svg
+      class="svg-icon"
+      width="100%"
+      preserveAspectRatio="xMidYMid meet"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <g :fill="iconColor">
+        <component :is="iconLoader" />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SvgIcon",
+  components: {},
+  props: {
+    iconType: {
+      type: String,
+      default: () => null
+    },
+    iconColor: {
+      type: String,
+      default: "currentColor"
+    },
+    width: {
+      type: [Number, String],
+      default: 18
+    },
+    height: {
+      type: [Number, String],
+      default: 18
+    }
+  },
+  computed: {
+    iconLoader() {
+      return () => import(`@/components/icons/${this.iconType}.vue`);
+    },
+    iconStyle() {
+      const { width, height } = this;
+      return {
+        width: `${width}px`,
+        height: `${height}px`
+      };
+    }
+  }
+};
+</script>
+
+<style></style>
