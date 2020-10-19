1851692796fda40caf19dea4b384d6880492d758
강희연 <k.h22y25n@gmail.com>
2020-10-19 16:58:44 +0900

    Add: webpack-demo에 tailwindcss 적용 예제 추가

---
+++ webpack/webpack-demo/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1192E8',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
